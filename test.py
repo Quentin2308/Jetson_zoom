40cf6fa18c6c78db1630d8e6fd567d36d527db6d
Quentin2308 <115085012+Quentin2308@users.noreply.github.com>
2022-10-25 15:08:38 +0200

    Update test.py

--- test.py
+++ test.py
@@ -35,8 +35,8 @@ try:
     serial_port.write(Commands.adress_set)
     temps=time.time() #stockage du tps actuel
     
-    while(time.time()-temps<10000): #10 secondes
-        serial_port.write(Commands.ZoomTele)
+    while(time.time()-temps<5000): #5 secondes
+        serial_port.write(Commands.ZoomWide)
     serial_port.write(Commands.ZoomStop)
     
     while True:
